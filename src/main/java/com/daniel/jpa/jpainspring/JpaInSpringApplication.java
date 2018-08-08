d2bc3e66cb4f7a375fe1e8b1600aca7f17ce7c3b
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-08-08 16:46:31 -0500

    first commit

---
+++ src/main/java/com/daniel/jpa/jpainspring/JpaInSpringApplication.java
@@ -0,0 +1,12 @@
+package com.daniel.jpa.jpainspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaInSpringApplication.class, args);
+	}
+}
